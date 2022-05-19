57d2ab251008a842a610998542a0a03189fe6a45
zloisupport <zlo1supp0rt@gmail.com>
2022-05-19 11:42:43 +0600

    Add project files.

---
+++ WpfApp4/ObservableObject.cs
@@ -0,0 +1,16 @@
+﻿
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace WpfApp4
+{
+    class ObservableObject : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged([CallerMemberName] string name =null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+    }
+}
